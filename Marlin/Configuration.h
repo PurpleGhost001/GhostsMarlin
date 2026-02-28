4d225b26784bf1e960ab4e1b3e5c81cb00c6f9d8
PurpleGhost001 <krxmtz@gmail.com>
2026-02-28 20:54:38 +0100

    PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -712,9 +712,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp 27.14
-    #define DEFAULT_Ki 2.76
-    #define DEFAULT_Kd 66.77
+    #define DEFAULT_Kp 24.86
+    #define DEFAULT_Ki 2.43
+    #define DEFAULT_Kd 63.64
   #endif
 #else
   #define BANG_MAX 255    // Limit hotend current while in bang-bang mode; 255=full current
