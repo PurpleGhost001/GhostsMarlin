92a6c4bafb08dcee07c2fdf5d47ee88030ed00ab
PurpleGhost001 <krxmtz@gmail.com>
2026-03-01 13:54:06 +0100

    more features

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1763,7 +1763,7 @@
 #define PROBE_OFFSET_ZMAX   2.5   // (mm)
 
 // Enable the M48 repeatability test to test probe accuracy
-//#define Z_MIN_PROBE_REPEATABILITY_TEST
+#define Z_MIN_PROBE_REPEATABILITY_TEST
 
 // Before deploy/stow pause for user confirmation
 //#define PAUSE_BEFORE_DEPLOY_STOW
